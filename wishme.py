b877242150a652eee4612e1946d2c8b260573205
Sumit Kumar <sumitkumar33@amityonline.com>
2025-10-21 20:44:16 +0530

    Refactor wishme function to return salutation instead of using speak directly

--- wishme.py
+++ wishme.py
@@ -1,13 +1,12 @@
 import datetime
-from speak import speak
 from config import *
 # Wishes Good Morning, Afternoon, Evening according to time
 def wishme():
     hour = int(datetime.datetime.now().hour)
     if 0 <= hour < 12:
-        speak("Good Morning!")
+        salutation = "Good Morning!"
     elif 12 <= hour < 18:
-        speak("Good Afternoon!")
+        salutation = "Good Afternoon!"
     else:
-        speak("Good Evening!")
-    speak(f"I am {Name}. How may I help you today? You can speak *exit* anytime for me to stop listening")
+        salutation = "Good Evening!"
+    return f"{salutation} I am {Name}. How may I help you today? You can speak *exit* anytime for me to stop listening"
