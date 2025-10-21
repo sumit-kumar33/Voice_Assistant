6f8d86ddfb3f0e4004822c83a2dfd7e8deef96b0
Sumit Kumar <sumitkumar33@amityonline.com>
2025-10-21 20:41:57 +0530

    separated search in a separate module

---
+++ search.py
@@ -0,0 +1,10 @@
+import webbrowser
+import urllib.parse
+from config import search_engine
+def search(query):
+    query = query.replace("search", "").replace(search_engine, "").strip()
+    if query:
+        webbrowser.open(f"https://{search_engine}.com/search?q=" + urllib.parse.quote_plus(query))
+    else:
+        return "Please specify what you want to search for"
+    return f"I found this on {search_engine}"
