26d5db0a8c827902c93281158892477735c5d85b
Sumit Kumar <sumitkumar33@amityonline.com>
2025-10-28 19:00:18 +0530

    Refactor take_command function to replace speak calls with logging and moved user input to main function

--- speech/take_command.py
+++ speech/take_command.py
@@ -1,18 +1,16 @@
 import speech_recognition
 from config_files.logs_config import *
-from speech.speak import *
 def take_command():
     r = speech_recognition.Recognizer()
     with speech_recognition.Microphone() as source:
-        speak("Listening")
+        logging.info("Listening...")
         r.pause_threshold = 1.5
         audio = r.listen(source)
     try:
-        speak("Recognising")
+        logging.info("Recognizing...")
         query = r.recognize_google(audio, language='en-in') # pyright: ignore[reportAttributeAccessIssue]
     except Exception:
         logging.exception("Failed to run take_command() function")
         messages.info(f"You: None")
         return None
-    messages.info(f"You: {query.lower()}")
     return query.lower()
